33397249134b5454d29e0663bdc6bc30f9ff994e
gabe305 <gabecruz718@gmail.com>
2023-11-25 18:40:20 -0600

    first push

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const fs = require("fs");
+const { v4: uuid } = require("uuid");
+
+const studentsFile = JSON.parse(fs.readFileSync("./data/students.json"));
+
+app.use(cors());
+app.use(express.json());
+
+//this is a post request to make a new student, sign up
+app.post("/SignUp", (req, res) => {
+    //this takes the request body from the front end and adds a unique id also
+    const newStudent = {
+        id: uuid(),
+        ...req.body,
+    };
+
+    //this adds a new student to the data file containing all students
+    let updatedStudents = [...studentsFile, newStudent];
+    fs.writeFileSync(
+        "./data/students.json",
+        JSON.stringify(updatedStudents)
+    );
+
+    //this sends a status to the front end and sends it the updated list of students
+    res.status(201).json(updatedStudents);
+});
+
+app.post("/LogIn", (req, res) => {
+    //this takes the request body from the front end and adds a unique id also
+
+    studentsFile.map((student) => {
+        console.log(student)
+        if (student.email === req.body.email) {
+            console.log("worked")
+            res.status(201).json(student);
+        }
+    })
+
+    //this sends a status to the front end and sends it the updated list of students
+});
+
+app.post("/EditStudent", (req, res) => {
+    const studentId = req.body.id;
+    const studentInfo = req.body;
+
+    let editStudent = studentsFile.filter(
+        (student) => student.id !== studentId
+    );
+    
+
+    const newData = editStudent.flat();
+    newData.push(studentInfo);
+
+    console.log(studentInfo);
+
+    fs.writeFileSync("./data/students.json", JSON.stringify(newData));
+
+    res.status(201).json(newData);
+});
+
+app.listen(8000, () => {
+    console.log(`Server is running on port 8000.`);
+});
